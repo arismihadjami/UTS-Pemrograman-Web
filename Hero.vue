28c3c77c792c7124888016f55bda215733c7c80e
arismihadjami <86295500+arismihadjami@users.noreply.github.com>
2021-11-15 15:25:22 +0800

    Add files via upload

---
+++ Hero.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Menu Makanan Enak, </strong> <br />untukMu!</h2>
+              <p>Segera pesan makanan favorit Kamu</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png.png" width="80%" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Menu Makanan Enak, </strong> <br />untukMu!</h2>
+              <p>Segera pesan makanan favorit Kamu</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
